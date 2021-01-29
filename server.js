be196154f075d448daa7290e8ff9d024960da34e
akliluMezene-wanos <aklilumezene@gmail.com>
2021-01-30 00:03:04 +0200

    added the rowCount and checked to 4 rows

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const client = new Client({
   })
   
   client.connect()
-  client.query('SELECT * FROM xclothes', (err, res) => {
+  client.query('SELECT * FROM manufacturer', (err, res) => {
     console.log(err, res.rowCount)
     client.end()
   })
